851ba6c7f762ab9fee59da35502f0166290ffaf4
Joe Thorley <joe@poissonconsulting.ca>
2025-01-07 14:47:24 -0800

    basic token working

--- R/token.R
+++ R/token.R
@@ -14,14 +14,14 @@
 #' }
 aq_token <- function(
     username = Sys.getenv("AQ_USERNAME"), 
-    password = Sys.getenv("BASEROW_PASSWORD")) {
+    password = Sys.getenv("AQ_PASSWORD")) {
   chk::chk_string(username)
   chk::chk_string(password)
   
-  token <- base_url() |>
-    file_path("user/token-auth/") |>
-    # httr2::request() |> 
-    # httr2::req_method("POST") |> 
+#  token <- base_url() |>
+  #  file_path("user/token-auth/") |>
+  #  httr2::request() |> 
+  #  httr2::req_method("POST") |> 
     # httr2::req_body_json(
     #   list(email = email,
     #        password = password)
@@ -30,7 +30,24 @@ aq_token <- function(
     # httr2::req_perform() |>
     # httr2::resp_body_json() |>
     # purrr::pluck("token") |>
-    identity()
+  #  identity()
+  #  
   
-  invisible(token)
+  response <- base_url() |>
+    httr2::request() |>
+    httr2::req_url_path_append("session") |>
+    httr2::req_body_form(
+      username = username,
+      encryptedPassword = password
+    ) |>
+    httr2::req_perform() |>
+    purrr::pluck(4) |>
+    purrr::pluck(15)
+  
+  token <- sub("AQAuthToken=([^;]+);.*", "\\1", response)
+  
+#  invisible(token)
+   token
 }
+
+
