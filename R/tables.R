f5a3e39547ced2884df0cf497891bae1a4f6c1ae
Joe Thorley <joe@poissonconsulting.ca>
2025-01-08 09:15:48 -0800

    getting dummy tables working

--- R/tables.R
+++ R/tables.R
@@ -12,10 +12,24 @@
 aq_tables <- function(token = aq_token()) {
   chk::chk_string(token)
   
- #  base_url() |>
- #    file_path(glue::glue("database/tables/database/")) |>
- #    request(token) |>
- #    print() |>
- # #   list_to_tibble()
- #    identity()
+  response <- base_url() |>
+    httr2::request() |>
+    httr2::req_method("GET") |>
+    httr2::req_url_path_append(session_append = "GetLocationData") |>
+    httr2::req_url_query(!!!list(LocationIdentifier = "LC_DRY_WQ06_TEMP")) |>
+    httr2::req_headers(Authorization = paste("Bearer", token)) |>
+    httr2::req_user_agent("aquariusapi") |>
+    httr2::req_perform(verbosity = 0) |>
+    httr2::resp_body_json()
+  
+  dplyr::tibble(
+    location_unique_id = response$UniqueId,
+    location_type = response$LocationType,
+    latitude = response$Latitude,
+    longitude = response$Longitude,
+    utc_offset = response$UtcOffset,
+    elevation = response$Elevation,
+    elevation_units = response$ElevationUnits,
+    location_description = response$Description
+  ) 
 }
