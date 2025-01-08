e68fb4ca3a3b855aaba388bb3fdfcdf142624c76
Joe Thorley <joe@poissonconsulting.ca>
2025-01-08 09:29:10 -0800

    - Added `get_location_data()`.

--- R/get-location-data.R
+++ R/get-location-data.R
@@ -7,19 +7,20 @@
 #'
 #' @examples
 #' \dontrun{
-#'  aq_tables()
+#'  aq_get_location_data("LC_DRY_WQ06_TEMP")
 #' }
-aq_tables <- function(token = aq_token()) {
+aq_get_location_data <- function(location, token = aq_token()) {
+  chk::chk_string(location)
   chk::chk_string(token)
   
   response <- base_url() |>
     httr2::request() |>
     httr2::req_method("GET") |>
     httr2::req_url_path_append(session_append = "GetLocationData") |>
-    httr2::req_url_query(!!!list(LocationIdentifier = "LC_DRY_WQ06_TEMP")) |>
+    httr2::req_url_query(!!!list(LocationIdentifier = location)) |>
     httr2::req_headers(Authorization = paste("Bearer", token)) |>
     httr2::req_user_agent("aquariusapi") |>
-    httr2::req_perform(verbosity = 0) |>
+    httr2::req_perform() |>
     httr2::resp_body_json()
   
   dplyr::tibble(
--- R/params.R
+++ R/params.R
@@ -8,13 +8,10 @@
 #' A string is a non-missing character scalar.
 #
 #' @inheritParams rlang::args_dots_empty
-#' @inheritParams httr2::req_perform
 #' @param token A string of the user's authentication token.
-#' @param location_identifiers A string of the name of the character vector column of location IDs.
+#' @param location A string of the location name.
 #' @param password A string of the user's password.
-#' @param time_series_ids A string of the name of the character vector column of time series IDs.
 #' @param username A string of the user name.
-#' @param x A data frame.
 
 #' @keywords internal
 #' @aliases parameters arguments args
